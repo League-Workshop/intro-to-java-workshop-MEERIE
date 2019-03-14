ca822a8f29482db1fd0e7584f390330ab03fb088
MEERIE <league@WTS-iMac-11.attlocal.net>
2019-03-13 18:27:25 -0700

    DAY 2

---
+++ src/section1/tashme_bros/tashme_bros.pde
@@ -0,0 +1,20 @@
+   
+PImage mustache; 
+PImage friend;
+ void setup() {
+   friend=loadImage("hi.jpg"); 
+   size(800, 600);
+friend.resize(800,600);
+}
+ void draw() {
+    background(friend);
+   
+   
+}
+ void setup(){
+   mustache = loadImage("hiu.jpg");
+
+
+
+   
+   
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -1,15 +1,19 @@
 package section2;
-
 import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
-
 public class MyFirstJavaProgram {
-	
 	public static void main(String[] args) {
+		Robot asphalt8 = new Robot();	
 		
-		// START HERE
-	
-		
-		
-	}
-}
+	asphalt8.setSpeed(5000);
+	asphalt8.penDown();
+	asphalt8.move(100);
+	asphalt8.turn(90);
+	asphalt8.move(100);
+	asphalt8.turn(90);
+	asphalt8.move(100);
+	asphalt8.turn(90);asphalt8.move(100);
+	asphalt8.turn(90);
+
+	asphalt8.penUp();
+}}
