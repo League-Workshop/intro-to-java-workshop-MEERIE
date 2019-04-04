ff2aeaa45c8b9642d2c32c559e77377d7cb39c8b
TangyTender <league@WorthyMini-5.attlocal.net>
2019-04-03 18:25:17 -0700

    DAY 5

--- src/section3/Greeter.java
+++ src/section3/Greeter.java
@@ -4,9 +4,11 @@ import javax.swing.JOptionPane;
 
 public class Greeter {
 	public static void main(String[] args) {
-		String name = JOptionPane.showInputDialog( "THIS IS CHUNGUS, "
-				+ "do u like peppa pig");
-		
+		String name = JOptionPane.showInputDialog( "THIS IS THE CHUNGUS CHALLENGE, "
+				+ "IT IS JUST LIKE THE MOMO CHALLENGE, INPUT YOUR NAME ! ! ! ! !");
+		String billybonjoeyouletsgo = JOptionPane.showInputDialog( "HI " + name  
+				+ " GIVE ME YOUR FORTNITE ACCOUNT  ! ! ! ! !");
+		JOptionPane.showMessageDialog (null,"OH U SILLY MAN BABY I HAVE TAKEN ALL YOUR V - BUCKS");
+		JOptionPane.showMessageDialog(null,"GET READY FOR LEVEL 2!");
 	}
-
 }
